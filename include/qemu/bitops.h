96ed5d4e6f0235b2e8968a2788e41f544ff13633
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2021-07-25 12:05:57 +0100

    bitops.h: revert db1ffc32dd ("qemu/bitops.h: add bitrev8 implementation")
    Commit db1ffc32dd ("qemu/bitops.h: add bitrev8 implementation") introduced
    a bitrev8() function to reverse the bit ordering required for storing the
    MAC address in the q800 PROM.
    
    This function is not required since QEMU implements its own revbit8()
    function which does exactly the same thing. Remove the extraneous
    bitrev8() function and switch its only caller in hw/m68k/q800.c to
    use revbit8() instead.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210725110557.3007-1-mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/qemu/bitops.h
+++ include/qemu/bitops.h
@@ -672,26 +672,4 @@ static inline uint8_t byte_pack_32(uint32_t x)
     return (x & 0xF);
 }
 
-/**
- * bitrev8:
- * @x: 8-bit value to be reversed
- *
- * Given an input value with bits::
- *
- *   ABCDEFGH
- *
- * return the value with its bits reversed from left to right::
- *
- *   HGFEDCBA
- *
- * Returns: the bit-reversed value.
- */
-static inline uint8_t bitrev8(uint8_t x)
-{
-    x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
-    x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
-    x = (x >> 4) | (x << 4) ;
-    return x;
-}
-
 #endif
