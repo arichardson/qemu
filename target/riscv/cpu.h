42b89b20b37ea9a68092b9e6b039c2968e52dc40
Atish Patra <atish.patra@wdc.com>
2022-06-20 16:15:57 -0700

    target/riscv: Support mcycle/minstret write operation
    mcycle/minstret are actually WARL registers and can be written with any
    given value. With SBI PMU extension, it will be used to store a initial
    value provided from supervisor OS. The Qemu also need prohibit the counter
    increment if mcountinhibit is set.
    
    Support mcycle/minstret through generic counter infrastructure.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-Id: <20220620231603.2547260-8-atishp@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    
    cheri-remergify-drop

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -120,7 +120,7 @@ typedef struct CPURISCVState CPURISCVState;
 #endif
 
 #define RV_VLEN_MAX 256
-#define RV_MAX_MHPMEVENTS 29
+#define RV_MAX_MHPMEVENTS 32
 #define RV_MAX_MHPMCOUNTERS 32
 
 FIELD(VTYPE, VLMUL, 0, 3)
@@ -129,7 +129,19 @@ FIELD(VTYPE, VEDIV, 8, 2)
 FIELD(VTYPE, RESERVED, 10, sizeof(target_ulong) * 8 - 11)
 FIELD(VTYPE, VILL, sizeof(target_ulong) * 8 - 1, 1)
 
+typedef struct PMUCTRState {
+    /* Current value of a counter */
+    target_ulong mhpmcounter_val;
+    /* Current value of a counter in RV32*/
+    target_ulong mhpmcounterh_val;
+    /* Snapshot values of counter */
+    target_ulong mhpmcounter_prev;
+    /* Snapshort value of a counter in RV32 */
+    target_ulong mhpmcounterh_prev;
+    bool started;
+} PMUCTRState;
 struct CPURISCVState {
+
 #ifdef TARGET_CHERI
     struct GPCapRegs gpcapregs;
 #else
@@ -303,13 +315,10 @@ struct CPURISCVState {
 
     target_ulong mcountinhibit;
 
-    /* PMU counter configured values */
-    target_ulong mhpmcounter_val[RV_MAX_MHPMCOUNTERS];
+    /* PMU counter state */
+    PMUCTRState pmu_ctrs[RV_MAX_MHPMCOUNTERS];
 
-    /* for RV32 */
-    target_ulong mhpmcounterh_val[RV_MAX_MHPMCOUNTERS];
-
-    /* PMU event selector configured values */
+    /* PMU event selector configured values. First three are unused*/
     target_ulong mhpmevent_val[RV_MAX_MHPMEVENTS];
 
 
