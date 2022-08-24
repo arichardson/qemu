1dcbdf4f346f278180ac583354771154212217e8
Atish Patra <atish.patra@wdc.com>
2022-08-24 15:16:59 -0700

    target/riscv: Add few cache related PMU events
    Qemu can monitor the following cache related PMU events through
    tlb_fill functions.
    
    1. DTLB load/store miss
    3. ITLB prefetch miss
    
    Increment the PMU counter in tlb_fill function.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-Id: <20220824221701.41932-4-atishp@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    
    cheri-remergify-drop

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -21,6 +21,7 @@
 #include "qemu/log.h"
 #include "qemu/main-loop.h"
 #include "cpu.h"
+#include "pmu.h"
 #include "exec/exec-all.h"
 #include "exec/log_instr.h"
 #include "tcg/tcg-op.h"
@@ -43,6 +44,8 @@
         (env)->dst = (env)->src;                                \
     } while (false)
 #endif
+
+#include "cpu_bits.h"
 
 int riscv_cpu_mmu_index(CPURISCVState *env, bool ifetch)
 {
@@ -1601,6 +1604,28 @@ static inline int rvfi_dii_check_addr(CPURISCVState *env, int ret, hwaddr *pa,
     return ret;
 }
 
+
+static void pmu_tlb_fill_incr_ctr(RISCVCPU *cpu, MMUAccessType access_type)
+{
+    enum riscv_pmu_event_idx pmu_event_type;
+
+    switch (access_type) {
+    case MMU_INST_FETCH:
+        pmu_event_type = RISCV_PMU_EVENT_CACHE_ITLB_PREFETCH_MISS;
+        break;
+    case MMU_DATA_LOAD:
+        pmu_event_type = RISCV_PMU_EVENT_CACHE_DTLB_READ_MISS;
+        break;
+    case MMU_DATA_STORE:
+        pmu_event_type = RISCV_PMU_EVENT_CACHE_DTLB_WRITE_MISS;
+        break;
+    default:
+        return;
+    }
+
+    riscv_pmu_incr_ctr(cpu, pmu_event_type);
+}
+
 bool riscv_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
                         MMUAccessType access_type, int mmu_idx,
                         bool probe, uintptr_t retaddr)
@@ -1732,6 +1757,7 @@ bool riscv_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
             }
         }
     } else {
+        pmu_tlb_fill_incr_ctr(cpu, access_type);
         /* Single stage lookup */
         ret = get_physical_address(env, &pa, &prot, address, NULL,
                                    access_type, mmu_idx, true, false, false);
