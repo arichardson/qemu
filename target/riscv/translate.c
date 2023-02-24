d6e26db377f594493c296e13fbdd1e4c4e04d26c
Christoph Muellner <cmuellner@linux.com>
2023-02-24 10:25:34 -0300

    target/riscv: implement Zicboz extension
    The RISC-V base cache management operation (CBO) ISA extension has been
    ratified. It defines three extensions: Cache-Block Management, Cache-Block
    Prefetch and Cache-Block Zero. More information about the spec can be
    found at [1].
    
    Let's start by implementing the Cache-Block Zero extension, Zicboz. It
    uses the cbo.zero instruction that, as with all CBO instructions that
    will be added later, needs to be implemented in an overlap group with
    the LQ instruction due to overlapping patterns.
    
    cbo.zero throws a Illegal Instruction/Virtual Instruction exception
    depending on CSR state. This is also the case for the remaining cbo
    instructions we're going to add next, so create a check_zicbo_envcfg()
    that will be used by all Zicbo[mz] instructions.
    
    [1] https://github.com/riscv/riscv-CMOs/blob/master/specifications/cmobase-v1.0.1.pdf
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Co-developed-by: Philipp Tomsich <philipp.tomsich@vrull.eu>
    Signed-off-by: Christoph Muellner <cmuellner@linux.com>
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Reviewed-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Message-ID: <20230224132536.552293-3-dbarboza@ventanamicro.com>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    This is a manual backport of upstream commit a939c500793 ("target/riscv:
    implement Zicboz extension").
    
    Update the extension definition and properties for 6.x.
    cpu_gpr[] is not used for cheri, use get_gpr to read a register value.

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -890,6 +890,7 @@ get_capmode_dependent_rmw_addr(DisasContext *ctx, int reg_num,
 #include "insn_trans/trans_rvh.c.inc"
 #include "insn_trans/trans_rvv.c.inc"
 #include "insn_trans/trans_rvb.c.inc"
+#include "insn_trans/trans_rvzicbo.c.inc"
 #include "insn_trans/trans_privileged.c.inc"
 
 /* Include the auto-generated decoder for 16 bit insn */
