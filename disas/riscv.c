5a25b4987bee26f2cb83a3489f9dd4611798cafa
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-22 08:29:57 -0700

    CHERI-RISC-V: add the gclen instruction
    The RISC-V standard renamed the CGetLen instruction to gclen and
    modified the encoding. This instruction reads the length of a capability
    and stores it in a register.
    
    Co-authored-by: Alexander Richardson <alexrichardson@google.com>

--- disas/riscv.c
+++ disas/riscv.c
@@ -570,6 +570,7 @@ typedef enum {
     rv_op_gcperm,
     rv_op_gchi,
     rv_op_gcbase,
+    rv_op_gclen,
 
     // Three operand
     rv_op_cspecialrw,
@@ -1338,6 +1339,7 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_gcperm] = { "gcperm", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_gchi] = { "gchi", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
     [rv_op_gcbase] = { "gcbase", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
+    [rv_op_gclen] = { "gclen", rv_codec_r, rv_fmt_rd_cs1, NULL, 0, 0, 0 },
 
     // capmode loads:
     [rv_op_clb] = { "clb", rv_codec_i, rv_fmt_rd_offset_cs1, NULL, 0, 0, 0 },
@@ -2025,6 +2027,7 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
                 case 0b00001: op = rv_op_gcperm; break;
                 case 0b00100: op = rv_op_gchi; break;
                 case 0b00101: op = rv_op_gcbase; break;
+                case 0b00110: op = rv_op_gclen; break;
                 }
                 break;
             case 73:  op = rv_op_modesw_cap; break;
