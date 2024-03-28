4b665ea894b129f0cdaf266d489d2cd4c0fc6c73
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-28 08:18:58 -0700

    CHERI-RISC-V: add the sceq instruction
    The RISC-V standard renamed the csetequalexact instruction to sceq and
    modified the encoding. This instruction compares two capabilities.
    
    Co-authored-by: Alexander Richardson <alexrichardson@google.com>

--- disas/riscv.c
+++ disas/riscv.c
@@ -598,6 +598,7 @@ typedef enum {
 
     rv_op_scaddr,
     rv_op_schi,
+    rv_op_sceq,
 
     // FP loads/store
     rv_op_cflw,
@@ -1386,6 +1387,7 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_caddi] = { "caddi", rv_codec_i, rv_fmt_cd_cs1_imm, NULL, 0, 0, 0 },
     [rv_op_scaddr] = { "scaddr", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
     [rv_op_schi] = { "schi", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
+    [rv_op_sceq] = { "sceq", rv_codec_r, rv_fmt_rd_cs1_cs2, NULL, 0, 0, 0 },
 
     // FP load store
     [rv_op_cflw] = { "cflw", rv_codec_i, rv_fmt_frd_offset_cs1, NULL, 0, 0, 0 },
@@ -2036,6 +2038,7 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
             case 48: op = rv_op_cadd; break;
             case 49: op = rv_op_scaddr; break;
             case 51: op = rv_op_schi; break;
+            case 52: op = rv_op_sceq; break;
             case 64:
                 switch ((inst >> 20) & 0b11111) {
                 case 0b00000: op = rv_op_gctag; break;
