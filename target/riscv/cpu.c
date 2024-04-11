abd1cfcb823a0666ddd6060356a533c81e83330f
Paul Buxton <paul.buxton@codasip.com>
2024-04-11 01:43:47 -0700

    CHERI-RISC-V: add STVECC CSR handling
    The standard renames STCC to STVECC.
    Add the read/write functions for CSRRW.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -756,7 +756,7 @@ static void riscv_cpu_reset(DeviceState *dev)
     set_max_perms_capability(&env->ddc, 0);
     null_capability(&env->utidc);
     // Supervisor mode trap handling
-    set_max_perms_capability(&env->stcc, 0);
+    set_max_perms_capability(&env->stvecc, 0);
     null_capability(&env->stdc);
     null_capability(&env->sscratchc);
     set_max_perms_capability(&env->sepcc, 0);
