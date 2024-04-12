ae5981c4fecd96158da0a5891a2f7399fc554541
Paul Buxton <paul.buxton@codasip.com>
2024-04-12 14:12:32 +0100

    Add SSCRATCHC CSR capability handling
    Adds the handling of SSCARTCHC to the CSR functions.
    Removes references to CheriSCR_SSCRATCHC

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -97,10 +97,6 @@ struct SCRInfo {
     [CheriSCR_UTIDC] = {.r = true, .w = true, .access = U_ASR_W, .name = "UTIDC"},
 
     [CheriSCR_STDC] = {.r = true, .w = true, .access = S_ASR_RW, .name = "STDC"},
-    [CheriSCR_SScratchC] = {.r = true,
-                            .w = true,
-                            .access = S_ASR_RW,
-                            .name = "SScratchC"},
     [CheriSCR_STIDC] = {.r = true, .w = true, .access = S_ASR_W, .name = "STIDC"},
 
     [CheriSCR_MTDC] = {.r = true, .w = true, .access = M_ASR_RW, .name = "MTDC"},
