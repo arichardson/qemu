7556a4d8b7ebf818a1aa34b764dcdb4e2ee17eed
Paul Buxton <paul.buxton@codasip.com>
2024-04-26 11:07:13 +0100

    Raise correct exception for permission violation.
    The permissions of CSR register is based on the execution mode of the PC (M/S/U) as well as the register range. If the mode is not correct we should raise the illegal instruction exception.

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -190,7 +190,7 @@ void HELPER(csrrw_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
 
     ret = check_csr_cap_permissions(env,csr,1);
     if (ret) {
-        riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+        riscv_raise_exception(env, -ret, GETPC());
     }
 
     rs_cap = *get_cap_in_gpregs(&env->gpcapregs,rs1);
@@ -219,7 +219,7 @@ void HELPER(csrrs_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
     if (rs1) {
         ret = check_csr_cap_permissions(env,csr,1);
         if (ret) {
-            riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+            riscv_raise_exception(env, -ret, GETPC());
         }
         rs_cap = *get_cap_in_gpregs(&env->gpcapregs,rs1);
     }
@@ -251,7 +251,7 @@ void HELPER(csrrc_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
     if(rs1) {
         ret = check_csr_cap_permissions(env,csr,1);
         if (ret) {
-            riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+            riscv_raise_exception(env, -ret, GETPC());
         }
         rs_cap = *get_cap_in_gpregs(&env->gpcapregs,rs1);
     }
@@ -282,7 +282,7 @@ void HELPER(csrrwi_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
     ret = check_csr_cap_permissions(env,csr,1);
 
     if (ret) {
-        riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+        riscv_raise_exception(env, -ret, GETPC());
     }
 
     csr_cap = csr_cap_info->read(env);
@@ -308,7 +308,7 @@ void HELPER(csrrsi_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
     if (rs1_val) {
         ret = check_csr_cap_permissions(env,csr,1);
         if (ret) {
-            riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+riscv_raise_exception(env, -ret, GETPC());
         }
     }
 
@@ -339,7 +339,7 @@ void HELPER(csrrci_cap)(CPUArchState *env, uint32_t csr, uint32_t rd,
     if (rs1_val) {
         ret = check_csr_cap_permissions(env,csr,1);
         if(ret){
-            riscv_raise_exception(env, RISCV_EXCP_CHERI, GETPC());
+            riscv_raise_exception(env, -ret, GETPC());
         }
     }
 
