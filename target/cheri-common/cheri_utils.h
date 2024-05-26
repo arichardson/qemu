3f0a6654a2a92db35bba63997d0037b918b7925e
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-26 13:54:09 +0200

    cheri: refactor capability field prints
    Refactor the prints that dump a capability field by field.
    
    Move some of the formatting from the print functions into the format
    defines. This makes it easier to remove fields that do not exist in cheri
    bakewell without affecting the v9 dumps.

--- target/cheri-common/cheri_utils.h
+++ target/cheri-common/cheri_utils.h
@@ -53,7 +53,7 @@
     (cr)->cr_tag, cap_is_sealed_with_type(cr), cap_get_all_perms(cr),          \
         cap_get_base(cr), cap_get_length_sat(cr)
 #define PRINT_CAP_FMTSTR_L2                                                    \
-    "o:" TARGET_FMT_lx " t:" TARGET_FMT_lx PRINT_CAP_FMT_EXTRA
+    "\n             |o:" TARGET_FMT_lx " t:" TARGET_FMT_lx PRINT_CAP_FMT_EXTRA
 #define PRINT_CAP_ARGS_L2(cr)                                                  \
     (target_ulong) cap_get_offset(cr),                                         \
         cap_get_otype_unsigned(cr) PRINT_CAP_ARGS_EXTRA(cr)
