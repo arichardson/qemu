bef38d88e67847c1f8303997f963a86828408c91
Martin Kaiser <martin.kaiser@codasip.com>
2024-05-26 13:54:09 +0200

    cheri: refactor capability field prints
    Refactor the prints that dump a capability field by field.
    
    Move some of the formatting from the print functions into the format
    defines. This makes it easier to remove fields that do not exist in cheri
    bakewell without affecting the v9 dumps.

--- accel/tcg/log_instr.c
+++ accel/tcg/log_instr.c
@@ -315,8 +315,8 @@ static inline void emit_text_reg(log_reginfo_t *rinfo)
 #else
     if (reginfo_is_cap(rinfo)) {
         if (reginfo_has_cap(rinfo))
-            qemu_log("    Write %s|" PRINT_CAP_FMTSTR_L1 "\n"
-                     "             |" PRINT_CAP_FMTSTR_L2 "\n",
+            qemu_log("    Write %s|" PRINT_CAP_FMTSTR_L1
+                     PRINT_CAP_FMTSTR_L2 "\n",
                      rinfo->name,
                      PRINT_CAP_ARGS_L1(&rinfo->cap),
                      PRINT_CAP_ARGS_L2(&rinfo->cap));
