1eee7496768931817a0817ade8df412c084912c6
Paul Buxton <paul.buxton@codasip.com>
2024-05-31 14:51:01 +0100

    Adjust jal capability checking.
    Update the capability checking when implementing cjal / cjalr to match the RiscV Cheri spec.

--- target/riscv/cheri-archspecific.h
+++ target/riscv/cheri-archspecific.h
@@ -132,12 +132,12 @@ static inline bool validate_jump_target(CPUArchState *env,
 {
     target_ulong base = cap_get_base(cap);
     unsigned min_insn_size = riscv_has_ext(env, RVC) ? 2 : 4;
-    if (!cap_is_in_bounds(cap, addr, min_insn_size)) {
-        raise_cheri_exception_branch_impl(env, CapEx_LengthViolation, regnum,
-                                          addr, retpc);
-    }
     if (!QEMU_IS_ALIGNED(base, min_insn_size)) {
         raise_cheri_exception_branch_impl(env, CapEx_UnalignedBase, regnum,
+                                          addr, retpc);
+    }
+    if (!cap_is_in_bounds(cap, addr, min_insn_size)) {
+        raise_cheri_exception_branch_impl(env, CapEx_LengthViolation, regnum,
                                           addr, retpc);
     }
     // XXX: Sail only checks bit 1 why not also bit zero? Is it because that is
