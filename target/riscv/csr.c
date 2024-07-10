f8838fb9c7defe6e114ca898165a385264281038
Paul Buxton <paul.buxton@codasip.com>
2024-07-10 14:26:35 +0000

    Update the commit with comment regarding CRE bits.
    The CRE bits are currently the only supported bits in the menv, senv and mseccfg register in our qemu implementation. Rather than attempting to do lots of masking here we will expect to integrate with however it is handled upstream when we come to merge.

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -1465,6 +1465,7 @@ static RISCVException write_mtinst(CPURISCVState *env, int csrno,
 static RISCVException read_menvcfg(CPURISCVState *env, int csrno, target_ulong *val)
 {
     if ((env->mseccfg & MSECCFG_CRE)) {
+        // at present the CRE bit is the only supported field in the register
         *val = env->menvcfg & MENVCFG_CRE;
     } else {
         *val = 0;
@@ -1484,6 +1485,7 @@ static RISCVException write_menvcfg(CPURISCVState *env, int csrno, target_ulong 
 static RISCVException read_senvcfg(CPURISCVState *env, int csrno, target_ulong *val)
 {
     if ((env->mseccfg & MSECCFG_CRE) && (env->menvcfg & MENVCFG_CRE)) {
+        // at present the CRE bit is the only supported field in the register
         *val = env->senvcfg & SENVCFG_CRE;
     } else {
         *val = 0;
