17ac691d538b52a6b533dfd9deeb7c4edf5ed7fe
Martin Kaiser <martin.kaiser@codasip.com>
2024-08-15 10:43:14 +0200

    cheri: remove capmode check for acperm
    The availabilty of the acperm instruction depends on cheri register
    access (the CRE bits), not on capability pointer mode.
    
    The decodetree definition does already do the correct check. Remove the
    invalid additional check in the helper function.
    
    Fixes: 75a27ba2b17 ("cheri: riscv: illegal instruction for acperm in int ptr mode")
    Fixes: 880a2d7228f ("cheri: riscv: replace candperm with acperm")

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -932,12 +932,6 @@ void CHERI_HELPER_IMPL(acperm(CPUArchState *env, uint32_t cd, uint32_t cs1,
 
     cap_register_t result = *cbp;
 
-    /* see the comment about capmode in the gcperm helper */
-    if (!cheri_in_capmode(env)) {
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-        return;
-    }
-
     if (!cap_is_unsealed(cbp)) {
         result.cr_tag = 0;
     }
