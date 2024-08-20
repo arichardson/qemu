4f0b8b0b00c3d1fd0e15573a8d7d8235e9018ddf
Paul Buxton <paul.buxton@codasip.com>
2024-08-20 13:07:31 +0100

    Add brick tracing library and wrapper to the build.
    Add optional linking against brick library.
    The library can either be found by setting the --brick-dir configuration variable. Or use the default /opt/brick by setting --enable-brick.
    Add in a C /C++ interface wrapper to enable calls from the logging code.
    The brick release can be found at https://gitlab.codasip.com/dv-infra/brick/brick/-/releases
    We do not directly include this in the build or in the repository as the brick library license is not compatible with QEMU for a binary release.
    It can only be distributed in such a way that users build it themselves and do not distribute.

---
+++ include/qemu/brick_wrapper.h
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-2.0
+// SPDX-FileCopyrightText: 2024 Codasip s.r.o.
+
+/* Implements a C wrapper around a C++ interface to the brick library.*/
+
+#ifdef __cplusplus
+#include <cstdint>
+#endif
+
+
+typedef enum{
+    U_MODE=0,
+    S_MODE,
+    H_MODE,
+    M_MODE,
+    D_MODE
+}CPU_PRIV_MODE;
+
+typedef enum{
+    INTEGER=0,
+    CAPABILITY,
+}CPU_CHERI_MODE;
+
+
+typedef struct _brick_track_event_
+{
+    uint32_t insn;
+    uint64_t pc;
+    uint64_t count;
+}brick_track_event;
+
+typedef struct _brick_track_reg_
+{
+    const char* regname;
+    uint64_t offset;
+    uint64_t pesbt;
+    bool tag_valid;
+    bool is_cap;
+}brick_track_reg;
+
+typedef struct _brick_track_cpu_state_{
+    CPU_PRIV_MODE privilege;
+    bool cheri_mode;
+    CPU_CHERI_MODE isamode;
+}brick_track_cpu_state;
+
+typedef struct _brick_track_mem_trnsn_{
+    uint8_t flags;
+    uint64_t addr;
+    uint8_t size;
+    uint64_t val;
+    uint64_t pesbt;
+}brick_track_mem_trnsn;
+
+#define MEM_LD 1
+#define MEM_ST 2
+#define MEM_CAP 4
+
+#ifndef __cplusplus
+void init_eventstream(int logfd);
+int output_track_event(brick_track_event *ev);
+void track_reg_write(brick_track_reg *reg_event);
+void track_cpu_state(brick_track_cpu_state *state);
+void track_mem_transaction(brick_track_mem_trnsn *trnsn);
+void close_eventstream(void);
+#endif 
