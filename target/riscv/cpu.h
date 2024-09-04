12a3d0b9bd959073d29335d835ff25d6580b17c8
Martin Kaiser <martin.kaiser@codasip.com>
2024-09-04 16:06:22 +0200

    riscv: cheri: pred_cre must return true for purecap mode
    There are several cheri instructions which are allowed in purecap mode
    and in hybrid mode if cre allows register access. In our decodetree
    definitions, these instructions depend on pred_cre.
    
    This means that pred_cre must return true if we are in purecap mode.
    
    The problem can be reproduced by running
    
    qemu-system-riscv64cheri -cpu codasip-a730,Xcheri_purecap=on -M hobgoblin-genesys2 -nographic -bios hello_world -d instr
    
    with a purecap hello_world application. Without this patch, we're stuck
    in the rom reset code.

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -898,6 +898,14 @@ riscv_csr_cap_ops *get_csr_cap_info(uint32_t csrnum);
 /* Do the CRE bits allow cheri access in the current CPU mode? */
 static inline bool riscv_cpu_mode_cre(CPURISCVState *env)
 {
+    /*
+     * CRE bits are defined only if Zcherihybrid is supported.
+     * For Zcheripurecap, cheri register access is always allowed.
+     */
+    if (!riscv_feature(env, RISCV_FEATURE_CHERI_HYBRID)) {
+        return true;
+    }
+
     if (env->mseccfg & MSECCFG_CRE) {
         /* CRE bits allow cheri in M mode */
         if (env->priv == PRV_M)
