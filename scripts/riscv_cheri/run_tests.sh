f7a8c5f2a8750953e6b7ff25110a8c0ff77f0a2c
Paul Buxton <paul.buxton@codasip.com>
2024-09-10 15:42:19 +0100

    Add script to run tests. Build_test_all.sh enables easily running tests from commandline. It will perform build and run_tests for all architectures unless one is specified as a parameter. The run_tests script will run both the qemu meson tests as well as the cheri specirfic imlemented as binaries in elfhome subdirectory.

---
+++ scripts/riscv_cheri/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TARGET=$1
+TARGET_LIST=${TARGET}-softmmu
+NINJA_TARGET=qemu-system-${TARGET}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_DIR=$(realpath ${SCRIPT_DIR}/../..)
+BUILD_DIR=${REPO_DIR}/build_${TARGET}
+MESON="/usr/bin/python3 -B $REPO_DIR/meson/meson.py"
+which meson && MESON=$(which meson)
+pushd $BUILD_DIR
+$MESON test --suite qemu
+
+pushd $SCRIPT_DIR
+qemu_bin=${REPO_DIR}/build_${TARGET}/${NINJA_TARGET} ./${TARGET}_test.sh -j -v -w
+mv $SCRIPT_DIR/${TARGET}_tests.xml $BUILD_DIR
+popd
+popd
+# at the end of this we will expect results in $BUILD_DIR/${TARGET}.xml and $BUILD_DIR/meson-logs/testlog.junit.xml
