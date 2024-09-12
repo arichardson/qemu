3be16560d834c93f09e25dcbf9745b3547ce9819
Paul Buxton <paul.buxton@codasip.com>
2024-09-12 14:31:13 +0100

    Address issues raised in review.

--- scripts/riscv_cheri/run_tests.sh
+++ scripts/riscv_cheri/run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 TARGET=$1
-TARGET_LIST=${TARGET}-softmmu
 NINJA_TARGET=qemu-system-${TARGET}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR=$(realpath ${SCRIPT_DIR}/../..)
@@ -10,10 +9,10 @@ MESON="/usr/bin/python3 -B $REPO_DIR/meson/meson.py"
 which meson && MESON=$(which meson)
 pushd $BUILD_DIR
 $MESON test --suite qemu
+popd
 
 pushd $SCRIPT_DIR
 qemu_bin=${REPO_DIR}/build_${TARGET}/${NINJA_TARGET} ./${TARGET}_test.sh -j -v -w
 mv $SCRIPT_DIR/${TARGET}_tests.xml $BUILD_DIR
-popd
 popd
 # at the end of this we will expect results in $BUILD_DIR/${TARGET}.xml and $BUILD_DIR/meson-logs/testlog.junit.xml
