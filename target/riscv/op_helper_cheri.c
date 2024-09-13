ecadbff59cb9529bae307269869df1b9aed1ba86
Paul Buxton <paul.buxton@codasip.com>
2024-09-13 10:25:22 +0100

    Pass index and type fields through to logging.
    Update the log instructions to include a register index and a type field. The type field is stored as additional flags in the logging register structure. The index is stored in the new index field of the structure.
    For non RiscV architectures try and use sensible values for index and type where possible.

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -365,10 +365,10 @@ static void lr_c_impl(CPUArchState *env, uint32_t dest_reg, uint32_t auth_reg,
     env->load_val = cursor;
     env->load_pesbt = pesbt;
     env->load_tag = tag;
-    log_changed_special_reg(env, "load_res", env->load_res);
-    log_changed_special_reg(env, "load_val", env->load_val);
-    log_changed_special_reg(env, "load_pesbt", env->load_pesbt);
-    log_changed_special_reg(env, "load_tag", (target_ulong)env->load_tag);
+    // log_changed_special_reg(env, "load_res", env->load_res);
+    // log_changed_special_reg(env, "load_val", env->load_val);
+    // log_changed_special_reg(env, "load_pesbt", env->load_pesbt);
+    // log_changed_special_reg(env, "load_tag", (target_ulong)env->load_tag);
     update_compressed_capreg(env, dest_reg, pesbt, tag, cursor);
 }
 
@@ -432,7 +432,7 @@ static target_ulong sc_c_impl(CPUArchState *env, uint32_t addr_reg,
     // an SC to any address, in between an LR and SC pair.
     // We do this regardless of success/failure.
     env->load_res = -1;
-    log_changed_special_reg(env, "load_res", env->load_res);
+    // log_changed_special_reg(env, "load_res", env->load_res);
     if (addr != expected_addr) {
         goto sc_failed;
     }
