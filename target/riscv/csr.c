4c70c4b422061d01e49da37810b3b59a1b4ed7a0
Paul Buxton <paul.buxton@codasip.com>
2024-09-17 10:23:25 +0100

    Use contents of register for logging csr write.
    The logging should reflect the state of the register after the write operation. Due to the way that fields may be ignored or read only using the source value will not match.

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -2418,8 +2418,11 @@ RISCVException riscv_csrrw(CPURISCVState *env, int csrno,
                 return ret;
             }
 #ifdef CONFIG_TCG_LOG_INSTR
-            if (csr_ops[csrno].log_update)
+            if (csr_ops[csrno].log_update){
+                csr_ops[csrno].read(env, csrno, &new_value);
                 csr_ops[csrno].log_update(env, csrno, new_value);
+
+            }
 #endif
         }
     }
