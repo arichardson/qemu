3e99cfd5db2eb1083f2dc0c014e879a96e3f06a4
Martin Kaiser <martin.kaiser@codasip.com>
2024-10-21 14:39:15 +0200

    riscv: cheri: bool propoerty for risc-v cheri v0.9.0
    Add a boolean property to control the new features in risc-v cheri v0.9.0.
    
    Default to off until the new features are mature and other software
    components have been updated to v0.9.0.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1119,6 +1119,7 @@ static Property riscv_cpu_properties[] = {
 #ifdef TARGET_CHERI_RISCV_STD
     DEFINE_PROP_BOOL("Xcheri_purecap", RISCVCPU, cfg.ext_cheri_purecap, false),
     DEFINE_PROP_BOOL("scmode_flip", RISCVCPU, cfg.scmode_flip, true),
+    DEFINE_PROP_BOOL("cheri_v090", RISCVCPU, cfg.cheri_v090, false),
 #endif
     DEFINE_PROP_STRING("priv_spec", RISCVCPU, cfg.priv_spec),
 
