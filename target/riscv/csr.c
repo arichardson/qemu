1dfa7cc5f6ae75add2df282f84f39d668f271c77
Martin Kaiser <martin.kaiser@codasip.com>
2024-10-31 01:11:40 -0700

    riscv: csr: simplify get_csr_cap_info
    Refactor get_csr_cap_info to iterate over the table. This makes it easier
    to remove a csr from the table or to add a csr somewhere in the middle.

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -2531,15 +2531,10 @@ static riscv_csr_cap_ops csr_cap_ops[] = {
 
 riscv_csr_cap_ops *get_csr_cap_info(int csrnum)
 {
-    switch (csrnum) {
-    case CSR_MSCRATCHC: return &csr_cap_ops[0];
-    case CSR_MTVECC: return &csr_cap_ops[1];
-    case CSR_STVECC: return &csr_cap_ops[2];
-    case CSR_MEPCC: return &csr_cap_ops[3];
-    case CSR_SEPCC: return &csr_cap_ops[4];
-    case CSR_SSCRATCHC: return &csr_cap_ops[5];
-    case CSR_DDC: return &csr_cap_ops[6];
-    default: return NULL;
+    for (int i = 0; i < ARRAY_SIZE(csr_cap_ops); i++) {
+      if (csr_cap_ops[i].reg_num == csrnum)
+          return &csr_cap_ops[i];
     }
+    return NULL;
 }
 #endif
