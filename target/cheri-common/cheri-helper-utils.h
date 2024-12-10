5eec3b0c392c0273d2262b6b8b1a7fc8cedf977a
Martin Kaiser <martin.kaiser@codasip.com>
2024-12-10 16:19:49 +0100

    cheri: pass auth cap to store_cap_to_memory_mmu_index
    Pass the register number of the authorizing capability down into
    store_cap_to_memory_mmu_index.
    
    store_cap_to_memory_mmu_index triggers the actual store operation. This
    is the correct place to clear the tag of the stored capability if the
    authorizing capability does not grant the C permission.
    
    (The approach is similar to the load operation, where the loaded
    capability's tag must be cleared unless the load is authorized by a
    capability with C permission.)

--- target/cheri-common/cheri-helper-utils.h
+++ target/cheri-common/cheri-helper-utils.h
@@ -279,7 +279,7 @@ static inline const char* cheri_type_str(CheriCapExcType type) {
 
 void store_cap_to_memory(CPUArchState *env, uint32_t cs, uint32_t cb,
                          target_ulong vaddr, uintptr_t retpc);
-void store_cap_to_memory_mmu_index(CPUArchState *env, uint32_t cs,
+void store_cap_to_memory_mmu_index(CPUArchState *env, uint32_t cs, uint32_t cb,
                                    target_ulong vaddr, uintptr_t retpc,
                                    int mmu_idx);
 
