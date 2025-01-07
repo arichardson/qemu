71188e97ffa8e321ee5b0930aa4f39ccbd257354
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-07 11:04:16 +0100

    cheri: process SL during capability store
    Process the SL permission when a capability is stored in memory.
    
    If the authorizing capability does not grant SL and the stored capability
    is "local" (CL == 0), it must be stored with the tag cleared.
    
    Internally, we use the full permission set (including EL, SL) even if
    Zcherilevels is not supported by the instructions. EL, SL are 0 in this
    case so they can be read as reserved bits. This means that we have to
    check if Zcherilevels is supported before we process SL.

--- target/cheri-common/op_helper_cheri_common.c
+++ target/cheri-common/op_helper_cheri_common.c
@@ -1661,6 +1661,7 @@ void store_cap_to_memory_mmu_index(CPUArchState *env, uint32_t cs,
 #endif
     bool tag = get_capreg_tag_filtered(env, cs);
 #if defined(TARGET_CHERI_RISCV_STD)
+    RISCVCPU *cpu = env_archcpu(env);
     const cap_register_t *cbp = get_capreg_or_special(env, cb);
     /*
      * For risc-v cheri, we have to store the capability with its tag cleared
@@ -1670,6 +1671,15 @@ void store_cap_to_memory_mmu_index(CPUArchState *env, uint32_t cs,
     if (!cap_has_perms(cbp, CAP_PERM_STORE_CAP)) {
         tag = false;
     }
+
+    /* If lvbits == 0, the format still has SL, but it's always 0 (reserved). */
+    if ((cpu->cfg.lvbits > 0) && !cap_has_perms(cbp, CAP_AP_SL)) {
+        const cap_register_t *csp = get_capreg_or_special(env, cs);
+        if (cap_get_level(csp) == 0) {
+            tag = false;
+        }
+    }
+
 #endif
     if (cs == NULL_CAPREG_INDEX) {
         tcg_debug_assert(pesbt_for_mem == 0 && "Wrong value for cnull?");
