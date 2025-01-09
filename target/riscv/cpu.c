9fffb1d5cd946ca97a067aea94c3c2ec4dad5cd3
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-09 08:25:35 +0100

    RVY 0.9.3: Add zcherilevels to isa string if enabled
    Add the zcherilevels extension to the isa string if this extension is
    enabled.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1289,6 +1289,7 @@ static void riscv_isa_string_ext(RISCVCPU *cpu, char **isa_str, int max_str_len)
         {"zcherihybrid", cpu->cfg.ext_zyhybrid},
         {"zcheripurecap", cpu->cfg.ext_cheri},
         {"zcheripte", cpu->cfg.cheri_pte },
+        {"zcherilevels", cpu->cfg.lvbits > 0 },
         ISA_EDATA_ENTRY(zish4add, ext_zish4add),
 #endif
     };
