c8141b9c5264a60858d0fbe662704769ad90d6f5
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-09 08:25:35 +0100

    riscv: cheri: add zcherilevels to isa string if enabled
    Add the zcherilevels extension to the isa string if this extension is
    enabled.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1267,6 +1267,7 @@ static void riscv_isa_string_ext(RISCVCPU *cpu, char **isa_str, int max_str_len)
         {"zcherihybrid", !cpu->cfg.ext_cheri_purecap},
         {"zcheripurecap", true},
         {"zcheripte", cpu->cfg.cheri_pte },
+        {"zcherilevels", cpu->cfg.lvbits > 0 },
 #endif
     };
 
