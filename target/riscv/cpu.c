b683fb9b8dc130eb7b5b60caff1e6352b559d10d
Martin Kaiser <martin.kaiser@codasip.com>
2025-01-27 10:46:30 +0100

    riscv: cbom_blocksize must be a power of two
    Check that the cache block size for cbom is a power of two.
    
    The code for aligning the start address to the cache block size
    requires this.
    
    Use the existing macro for this check.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1025,6 +1025,7 @@ static void riscv_cpu_realize(DeviceState *dev, Error **errp)
         set_misa(env, env->misa_mxl, ext);
     }
 
+    CHK_BLK_POW2(cbom_blocksize);
     CHK_BLK_POW2(cboz_blocksize);
 
 #ifdef TARGET_CHERI
