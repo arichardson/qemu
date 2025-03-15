20d070926d9a2e5a466ad0a0cf31469a65859633
Stuart Menefy <stuart.menefy@codasip.com>
2025-03-15 10:17:50 +0000

    target/riscv: Switch Codasip A730 to privileged spec version 1.12
    Now qemu has the infrastructure to support privileged spec version 1.12
    we can switch to the correct version.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -2410,6 +2410,9 @@ RISCVException riscv_csr_accessible(CPURISCVState *env, int csrno,
     int read_only = get_field(csrno, 0xC00) == 3;
     int csr_min_priv = csr_ops[csrno].min_priv_ver;
 
+    if (env->priv_ver < csr_min_priv) {
+        return RISCV_EXCP_ILLEGAL_INST;
+    }
     /* check privileges and return RISCV_EXCP_ILLEGAL_INST if check fails */
 #if !defined(CONFIG_USER_ONLY)
     int effective_priv = env->priv;
@@ -2487,10 +2490,6 @@ RISCVException riscv_csrrw(CPURISCVState *env, int csrno,
                                        /*regnum=*/0, 0, true, retpc);
 #endif
         return ret;
-    }
-
-    if (env->priv_ver < csr_min_priv) {
-        return RISCV_EXCP_ILLEGAL_INST;
     }
 
     /* execute combined read/write operation if it exists */
