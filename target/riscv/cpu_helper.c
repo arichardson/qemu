7a68da0043febce19ad6778da5cbdb2779f0e1e8
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2025-04-17 05:38:37 -0700

    cheri-riscv: Fix handling of stval2 for MMU exceptions.
    Update the value of stval2 correctly when a CHERI fault occurs as
    a result of PTE bits.

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -1547,7 +1547,7 @@ void riscv_cpu_do_interrupt(CPUState *cs)
         riscv_log_instr_csr_changed(env, CSR_HTVAL);
 
 #ifdef TARGET_CHERI_RISCV_STD_093
-        if (cause == RISCV_EXCP_CHERI) {
+        if (cause == RISCV_EXCP_CHERI || write_tval) {
             env->stval2 = cheri_exc_info;
             riscv_log_instr_csr_changed(env, CSR_STVAL2);
         }
