c5fa2b768d1dac18a3be614262774905dbcf9543
Paul Buxton <paul.buxton@codasip.com>
2025-09-12 14:35:05 +0100

    Add  zicboz to default A730 support

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -230,6 +230,7 @@ static void rv64_codasip_a730_cpu_init(Object *obj)
     qdev_prop_set_bit(DEVICE(obj), "mmu", true);
     qdev_prop_set_bit(DEVICE(obj), "pmp", false);
     qdev_prop_set_bit(DEVICE(obj), "zicbom", true);
+    qdev_prop_set_bit(DEVICE(obj), "zicboz", true);
     qdev_prop_set_bit(DEVICE(obj), "x-zba", true);
     qdev_prop_set_bit(DEVICE(obj), "x-zbb", true);
     qdev_prop_set_bit(DEVICE(obj), "x-zbs", true);
